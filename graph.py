8bebf3b94a39ecec855322a9be038351052bad61
jjaemin72 <soohyunlee124@gmail.com>
2024-08-29 16:04:05 -0400

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,52 @@
+# imports important packages similar to "#include" in C
+import numpy as np #this has useful data structures like arrays which will be discussed more later
+from matplotlib import pyplot as plt #this is for plotting nicely
+import sys, os
+
+if len(sys.argv) < 2:
+    print("Usage: python graph.py <filename>...")
+    exit(1)
+
+graph_paths = sys.argv[1:]
+print("Graphing file(s): ", graph_paths)
+
+# This will import the data we exported from C
+# in the place of "trajectory.txt" you should use your the name you gave your text file containing
+# the "data" from your simulation, which can be the full path if you do not want to store the data 
+# in the same directory as this program.
+graph_datas = [np.loadtxt(each) for each in graph_paths]
+
+# since we don't know how this was imported, I will just check by asking the program to tell me the 
+# dimensions of trajectory
+# print ("number of dimensions: ",trajectory.ndim)
+# print ("shape: ", trajectory.shape)
+
+
+# Now we know how the array "trajectory" is shaped, so we can plot the trajectory!
+# If the axes come out inverted, you can always switch the contents in the plot function
+
+
+colors = ["red", "blue", "green", "orange", "purple", "black", "yellow", "pink", "brown", "gray"]
+rgbs = [
+    (1, 0, 0),
+    (0, 0, 1),
+    (0, 1, 0),
+    (1, 0.5, 0),
+    (0.5, 0, 1),
+    (0, 0.5, 1),
+    (0.5, 1, 0),
+    (1, 0, 0.5),
+    (0, 1, 0.5),
+    (0.5, 0.5, 0.5),
+]
+for i, each in enumerate(graph_datas):
+    plt.plot(
+        each[:,0],
+        each[:,1],
+        color=rgbs[i] + (0.8,),
+        label=graph_paths[i].split("/")[-1].split(".")[0]
+    )
+
+plt.legend(loc='upper right')
+
+plt.show()
